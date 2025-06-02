f15f37df390c4f765b3abdd8b983a69029dd5024
jerryleee1 <25707264@qq.com>
2025-06-02 22:30:32 +0800

    Update Posters.vue

--- src/components/Posters.vue
+++ src/components/Posters.vue
@@ -126,12 +126,12 @@ function getMonth(time) {
     left: 5rem;
     display: flex;
     align-items: center;
-    font-size: 14rem;
+    font-size: 18rem;
     gap: 3rem;
 
     .love {
-      width: 14rem;
-      height: 14rem;
+      width: 18rem;
+      height: 18rem;
       margin-right: 5rem;
     }
   }
